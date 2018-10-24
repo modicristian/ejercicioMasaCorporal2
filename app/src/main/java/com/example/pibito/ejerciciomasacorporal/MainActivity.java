94273bed2f536f8ef7cd5fca7fef4cc3b6ac6151
modicristian <41091536+modicristian@users.noreply.github.com>
2018-10-24 19:55:55 -0300

    final de layout 1

--- app/src/main/java/com/example/pibito/ejerciciomasacorporal/MainActivity.java
+++ app/src/main/java/com/example/pibito/ejerciciomasacorporal/MainActivity.java
@@ -2,12 +2,67 @@ package com.example.pibito.ejerciciomasacorporal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Toolbar toolbar;
+    private EditText txtPeso, txtAltura , txtEdad;
+    private Spinner spinerGenero;
+    private Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtPeso = findViewById(R.id.txtPeso);
+        txtAltura = findViewById(R.id.txtAltura);
+        txtEdad = findViewById(R.id.txtEdad);
+
+        spinerGenero = findViewById(R.id.spinnerGenero);
+
+        btnCalcular = findViewById(R.id.btnCalcular);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(false);
+        getSupportActionBar().setTitle("CALCULAR LA MASA CORPORAL");
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.salir:
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.guardar:
+                break;
+        }
+        return true;
+
+
     }
 }
+
